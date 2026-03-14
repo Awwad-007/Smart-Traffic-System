43c137b9c5c7bf44c42bc791899379924401f3db
Awwad-007 <githubgm007@gmail.com>
2026-03-14 15:19:15 +0530

    Added initial traffic signal locations

--- traffic_schema.sql
+++ traffic_schema.sql
@@ -1,2 +1,9 @@
 USE SmartTrafficDB;
-SHOW TABLES;
+
+-- Clear previous test commands
+-- Add actual data
+INSERT INTO signals (location_name, current_state) 
+VALUES ('Silk Board Junction', 'RED'), ('Indiranagar 100ft Rd', 'GREEN');
+
+-- See the data
+SELECT * FROM signals;
