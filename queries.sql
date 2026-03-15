5a58db2e464a4c9146d9a053ed1db8a5934644ca
Awwad-007 <githubgm007@gmail.com>
2026-03-15 16:47:34 +0530

    Added Vehical Funstionality

--- queries.sql
+++ queries.sql
@@ -1,10 +1,23 @@
 USE SmartTrafficDB;
 
+-- 1. This adds vehicles ONLY if they aren't already there
+INSERT IGNORE INTO vehicles (license_plate, vehicle_type) 
+VALUES 
+('KA-01-HH-1234', 'PRIVATE'),
+('KA-03-EM-911', 'EMERGENCY'),
+('KA-05-B-5555', 'BUS');
+
+-- 2. This links a vehicle to a signal (Creating a "Log")
+-- We are saying: Vehicle 2 (Emergency) just passed Signal 1 (Silk Board)
+INSERT INTO trafficlogs (signal_id, vehicle_id) 
+VALUES (1, 2);
+
+-- 3. Now, let's see EVERYTHING combined
 SELECT 
-    signals.location_name, 
-    vehicles.license_plate, 
-    vehicles.vehicle_type, 
-    trafficlogs.passing_time
-FROM trafficlogs
-JOIN signals ON trafficlogs.signal_id = signals.signal_id
-JOIN vehicles ON trafficlogs.vehicle_id = vehicles.vehicle_id;
+    v.license_plate, 
+    v.vehicle_type, 
+    s.location_name, 
+    l.passing_time
+FROM trafficlogs l
+JOIN vehicles v ON l.vehicle_id = v.vehicle_id
+JOIN signals s ON l.signal_id = s.signal_id;
